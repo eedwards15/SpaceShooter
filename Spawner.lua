99f8dfe37f5124640fa74798e72960502d66de85
Eddie <alexwerewolf2009@gmail.com>
2020-02-11 23:03:57 -0500

    Refactor

---
+++ Spawner.lua
@@ -0,0 +1,35 @@
+enemy = require('enemy')
+utils = require('helpers.utils')
+
+Spawner = {}
+Spawner.CoolDown = 5
+Spawner.Enemies = {}
+
+function Spawner.Load()
+    Spawner.sprite = love.graphics.newImage("images/enemy.png")
+end 
+
+function Spawner.Update()
+    Spawner.CoolDown = Spawner.CoolDown - .05
+    Spawner.CreateEnemies()
+end
+
+
+function Spawner.CreateEnemies()
+    if Spawner.CoolDown <= 0 then 
+        local x = math.random(Spawner.sprite:getWidth()/2, love.graphics.getWidth() - Spawner.sprite:getWidth())
+        local y = math.random(Spawner.sprite:getHeight(), love.graphics.getHeight()/2)
+        local newEnemy = enemy:new(x, y)
+        table.insert(Spawner.Enemies , newEnemy)
+        Spawner.CoolDown= 5
+    end 
+
+end
+
+function Spawner.Draw()
+    for k, v in pairs(Spawner.Enemies) do
+        love.graphics.draw(Spawner.sprite, v:getX(),v:getY(), utils.degreesToRadians(180),nil,nil,Spawner.sprite:getWidth()/2, Spawner.sprite:getHeight()/2)
+    end
+end 
+
+return Spawner
--- main.lua
+++ main.lua
@@ -1,26 +1,27 @@
 utils = require("helpers.utils")
-enemy = require("enemy")
+Spawner = require("Spawner")
 
 GameState = {}
 
 function love.load()
+    GameState.Score = 0 
+    myFont = love.graphics.newFont(40)
+
     sprite = {}
     sprite.player = love.graphics.newImage("images/player_90_72.png")
-    sprite.enemy = love.graphics.newImage("images/enemy.png")
     sprite.bullet = love.graphics.newImage("images/missile.png")
 
     screenWidth = love.graphics.getWidth()
     screenHeight = love.graphics.getHeight()
    
-    Enemies = {}
-    EnemyCoolDown = 5
+    Spawner.Load()
 
     player = {}
     player.imageHeight = 72
     player.imageWidth = 90
     player.x =  screenWidth / 2
     player.y =  love.graphics.getHeight() - player.imageHeight
-    player.speed = 300 
+    player.speed = 350 
     player.shots = {}
     player.coolDown = 1
     player.shootCoolDown =.05
@@ -29,8 +30,7 @@ end
 
 function love.update(dt)
      player.coolDown = player.coolDown - player.shootCoolDown
-     EnemyCoolDown = EnemyCoolDown - .05 
-
+    --  EnemyCoolDown = EnemyCoolDown - .05 
 
     if love.keyboard.isDown("right") and player.x < (screenHeight + player.imageWidth) then
         player.x = player.x + (player.speed * dt)
@@ -52,7 +52,6 @@ function love.update(dt)
         shoot(dt)
     end 
 
-
     for k, shot in pairs(player.shots) do
         shot.y = shot.y - 10
         if shot.y < 0 then 
@@ -61,47 +60,37 @@ function love.update(dt)
     end
 
     for i,shot in ipairs(player.shots) do
-        for j,e in ipairs(Enemies) do
-          if utils.distanceBetween(e.x, e.y, shot.x, shot.y) < 70 then
+        for j,e in ipairs(Spawner.Enemies) do
+          if utils.distanceBetween(e.x, e.y, shot.x, shot.y) < (Spawner.sprite:getHeight()/2) then
             table.remove(player.shots, i)
-            table.remove(Enemies,j)
+            table.remove(Spawner.Enemies,j)
+            GameState.Score = GameState.Score + 10
           end
         end
     end
-
-    SpawnEmemies()
-
+    Spawner.Update()
 end
 
 function love.draw()
     love.graphics.draw(sprite.player, player.x, player.y)
     
+    love.graphics.setFont(myFont)
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.print("Score: " .. GameState.Score)
+
     for k, v in pairs(player.shots) do
         love.graphics.draw(sprite.bullet, v.x, v.y, nil, nil,nil, sprite.bullet:getWidth()/2, sprite.bullet:getHeight()/2)
     end
 
-    for k, v in pairs(Enemies) do
-        love.graphics.draw(sprite.enemy, v:getX(),v:getY(), utils.degreesToRadians(180),nil,nil, sprite.enemy:getWidth()/2, sprite.enemy:getHeight()/2)
-    end
-
+    Spawner.Draw()
 end
 
 function love.keypressed( key, scancode, isrepeat )
     if key == "s" then 
-        SpawnEmemies()
+        Spawner.CreateEnemies()
     end 
 end
 
-function SpawnEmemies()
-    if EnemyCoolDown <= 0 then 
-        local x = math.random(sprite.enemy:getWidth()/2, screenWidth - sprite.enemy:getWidth())
-        local y = math.random(sprite.enemy:getWidth(), screenHeight/2)
-        local newEnemy = enemy:new(x, y)
-        table.insert(Enemies, newEnemy)
-        EnemyCoolDown = 5
-    end 
-
-end
 
 function shoot(dt)
     if  player.coolDown <= 0 then 
